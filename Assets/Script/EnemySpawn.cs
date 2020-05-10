a05b1258cf016ec6a5cb3e0a7a28c4ff43fff041
PrimandaIJ <18523226@students.uii.ac.id>
2020-05-10 21:17:09 +0700

    first commit

---
+++ Assets/Script/EnemySpawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawn : MonoBehaviour {
+
+	public GameObject[] cars;
+	int carNo;
+	public float maxPos = 7f;
+	public float delayTimer = 0.5f;
+	float timer;
+
+	// Use this for initialization
+	void Start () {
+		timer = delayTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (timer <= 0){
+
+		Vector3 carPos = new Vector3(Random.Range(-7f,7f), transform.position.y, transform.position.z);
+		carNo = Random.Range (0,4);
+		Instantiate (cars[carNo], carPos, transform.rotation);
+		timer = delayTimer;
+		}
+	}
+}
---
+++ Assets/Script/carScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class carScript : MonoBehaviour {
+
+	public float carSpeed;
+	public float maxPos = 7.3f;
+	Vector3 position;
+	public uiManager ui;
+
+	void Awake(){
+
+	}
+
+	void Start () {
+		// ui = GetComponent<uiManager> ();
+		position = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position.x += Input.GetAxis ("Horizontal") * carSpeed * Time.deltaTime;
+
+		position.x = Mathf.Clamp (position.x, -7.3f, 7.3f);
+
+		transform.position = position;
+	}
+
+	void OnCollisionEnter2D(Collision2D col) {
+		if (col.gameObject.tag == "EnemyCar") {
+			Destroy (gameObject);
+			ui.gameOverActivated();
+		}
+	}
+}
